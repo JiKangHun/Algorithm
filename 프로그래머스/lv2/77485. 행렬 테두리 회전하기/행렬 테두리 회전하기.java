834f279b9b65434488dbd85458fcda111efa5829
JiKangHun <97156468+JiKangHun@users.noreply.github.com>
2022-12-28 21:26:47 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 39.65 ms, Memory: 93.7 MB -BaekjoonHub

--- 프로그래머스/lv2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
+++ 프로그래머스/lv2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -1,63 +1,6 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(int rows, int columns, int[][] queries) {
-        
-        int[] answer = new int[queries.length];
-        int[][] arr = new int[rows+1][columns+1];
-        int num = 1;
-        for(int i=1; i<rows+1; i++){
-            for(int j=1; j<columns+1; j++){
-                arr[i][j] = num++;
-            }
-        }
-        
-        int[] dx = {1,0,-1,0};
-        int[] dy = {0,1,0,-1};
-        
-        for(int i=0; i<queries.length; i++){
-            int x1 = queries[i][0];
-            int y1 = queries[i][1];
-            int x2 = queries[i][2];
-            int y2 = queries[i][3];
-            
-            int tmp = arr[x1][y1];
-            int d = 0;
-            
-            Queue<int[]> q = new LinkedList<>();
-            q.offer(new int[] {x1,y1});
-            int min = Integer.MAX_VALUE;
-            
-            while(true) {
-                int[] cur = q.poll();
-                int x = cur[0];
-                int y = cur[1];
-                
-                if(arr[x][y] < min) {
-                    min = arr[x][y];
-                    answer[i] = min;
-                }
-                
-                int nx = x + dx[d];
-                int ny = y + dy[d];
-                
-                
-                if(nx>x2 || ny >y2 || nx < x1 || ny < y1) d++;                                            
-                
-                nx = x + dx[d];
-                ny = y + dy[d];
-                
-                if(nx == x1 && ny == y1){
-                    arr[x][y] = tmp;
-                    break;
-                }
-                
-                arr[x][y] = arr[nx][ny];
-                q.offer(new int[] {nx, ny});
-            }
-        }
-   
-        
+        int[] answer = {};
         return answer;
     }
 }
