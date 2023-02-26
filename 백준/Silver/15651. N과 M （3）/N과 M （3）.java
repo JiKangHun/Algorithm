5fbbe5c58c8cbc93f9f960089a4e738a81ee7081
JiKangHun <97156468+JiKangHun@users.noreply.github.com>
2023-02-26 11:42:14 +0900

    [Silver III] Title: N과 M (3), Time: 500 ms, Memory: 262716 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,36 +1,36 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
 
-public class Main {
-	
-	static int[] num;
-	static StringBuilder sb = new StringBuilder();
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		num = new int[M];
-		perm(0, N, M);
-		System.out.println(sb);
-	}
+public class Main{
 
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws Exception {
 
-	private static void perm(int cnt, int N, int M) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[] num = new int[M];
 
-		if(cnt==M) {
-			
-			for(int i=0; i<M; i++) {
-				sb.append(num[i]+" ");
-			}
-			sb.append("\n");
-			return;
-		}
-		
-		for(int i=1; i<=N; i++) {
-			num[cnt] = i;
-			perm(cnt+1, N, M);
-		}
-	}
+        perm(0, N, M, num);
+        System.out.println(sb);
+
+    }
+
+    public static void perm(int cnt, int N, int M, int[] num){
+
+        if(cnt == M){
+            for(int pick : num){
+                sb.append(pick+" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=1; i<=N; i++){
+            num[cnt] = i;
+            perm(cnt+1, N, M, num);
+        }
+    }
 }
