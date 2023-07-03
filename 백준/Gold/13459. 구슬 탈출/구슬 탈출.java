836e471aa2ac936c4d24bf1eb63ccfdc13f0f262
JiKangHun <97156468+JiKangHun@users.noreply.github.com>
2023-07-03 11:30:24 +0900

    [Gold I] Title: 구슬 탈출, Time: 76 ms, Memory: 11584 KB -BaekjoonHub

--- 백준/Gold/13459. 구슬 탈출/구슬 탈출.java
+++ 백준/Gold/13459. 구슬 탈출/구슬 탈출.java
@@ -2,13 +2,10 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-
-// 3시간 10분
-
 public class Main {
 	
-	static int[] dr = {-1,0,1,0};
-	static int[] dc = {0,-1,0,1};
+	static int[] dr = {-1,1,0,0};
+	static int[] dc = {0,0,-1,1};
 	static int N;
 	static int M;
 	static int answer = Integer.MAX_VALUE;
@@ -39,131 +36,112 @@ public class Main {
 			}
 		}		
 		
-		dfs(0, 0, map, redLoc, blueLoc);
-		dfs(0, 1, map, redLoc, blueLoc);
-		dfs(0, 2, map, redLoc, blueLoc);
-		dfs(0, 3, map, redLoc, blueLoc);
+		for(int d=0; d<4; d++) {
+			boolean[] visited = new boolean[11111];
+			int chk = visitCal(redLoc[0], redLoc[1], blueLoc[0], blueLoc[1]);
+			visited[chk] = true;
+			dfs(0, d, map, redLoc, blueLoc, visited);
+		}
 		
 		answer = answer == Integer.MAX_VALUE ? 0 : 1;
 		System.out.println(answer);
 		
 	}
 
-	private static void dfs(int cnt, int d, char[][] map, int[] redLoc, int[] blueLoc) {
-		
-//		System.out.println("cnt: "+cnt);
-//		
-//		for(int i=0; i<N; i++) {
-//			for(int j=0; j<M; j++) {
-//				System.out.print(map[i][j]);
-//			}
-//			System.out.println();
-//		}
-//		System.out.println();
-		
+	private static void dfs(int cnt, int d, char[][] map, int[] redLoc, int[] blueLoc, boolean[] visited) {
 		
 		if(cnt >= answer) return;
 		if(cnt == 10) return;
+
+		int[] newRedLoc = {redLoc[0], redLoc[1]};
+		boolean isRedGoal = move(map, newRedLoc, d);
 		
-		int rR = redLoc[0];
-		int rC = redLoc[1];
-//		System.out.print("curR: "+rR+" ");
-//		System.out.println("curC: "+rC);
+		int[] newBlueLoc = {blueLoc[0], blueLoc[1]};
+		boolean isBlueGoal =  move(map, newBlueLoc, d);
 		
-		boolean aheadB = false;
-		boolean isReadGoal = false;
 		
-		while(true) {
-			
-			int nr = rR + dr[d];
-			int nc = rC + dc[d];
-			
-//			System.out.print("nr: "+nr+" ");
-//			System.out.println("nc: "+nc);
-			if(map[nr][nc] == '#') break;
-			if(map[nr][nc] == 'O') {
-				isReadGoal = true;
-				break;
-			}
-			if(map[nr][nc] == 'B') aheadB = true;
-			
-			rR = nr;
-			rC = nc;
-		}
-		
-		if( aheadB ) {
-			if(d == 0 ) {
-				rR += 1;
-			}else if( d == 1) {
-				rC += 1;
-			}else if( d == 2) {
-				rR -= 1;
-			}else {
-				rC -= 1;
-			}
-		}
-		
-		int bR = blueLoc[0];
-		int bC = blueLoc[1];
-		boolean aheadR = false;
-		boolean isBlueGoal = false;
-		
-		while(true) {
-			
-			int nr = bR + dr[d];
-			int nc = bC + dc[d];
-			
-			if(map[nr][nc] == '#') break;
-			if(map[nr][nc] == 'O') {
-				isBlueGoal = true;
-				break;
-			}
-			if(map[nr][nc] == 'R') aheadR = true;
-			
-			bR = nr;
-			bC = nc;
-			
-		}
-		
-		if( aheadR ) {
-			if(d == 0 ) {
-				bR += 1;
-			}else if( d == 1) {
-				bC += 1;
-			}else if( d == 2) {
-				bR -= 1;
-			}else {
-				bC -= 1;
-			}
-		}
-		
-		if(!isBlueGoal && isReadGoal ) {
+		if(!isBlueGoal && isRedGoal ) {
 			answer = Math.min(answer, cnt+1);
 			return;
 		}else if(isBlueGoal) {
 			return;
 		}
 		
-		int[] newRedLoc = {rR, rC};
-		int[] newBlueLoc = {bR, bC};
+		int newChk = visitCal(newRedLoc[0], newRedLoc[1], newBlueLoc[0], newBlueLoc[1]);
+		if(visited[newChk])return;
 		
 		// 다음 이동으로 보내기
 		map[redLoc[0]][redLoc[1]] = '.';
 		map[blueLoc[0]][blueLoc[1]] = '.';
 		
-		map[rR][rC] = 'R';
-		map[bR][bC] = 'B';
-//		System.out.println("bR: "+bR+"bC: "+bC);
+		map[newBlueLoc[0]][newBlueLoc[1]] = 'B';
+		map[newRedLoc[0]][newRedLoc[1]] = 'R';
+		
+		visited[newChk] = true;
 		
 		for(int i=0; i<=3; i++) {
 			
-			dfs(cnt+1, i, map, newRedLoc, newBlueLoc);
+			if(d<=1 && i<=1) continue;
+			if(d>1 && i>1) continue;
+			dfs(cnt+1, i, map, newRedLoc, newBlueLoc, visited);
 		}
 		
-		map[rR][rC] = '.';
-		map[bR][bC] = '.';
+		visited[newChk] = false;
+		
+		map[newRedLoc[0]][newRedLoc[1]] = '.';
+		map[newBlueLoc[0]][newBlueLoc[1]] = '.';
 		
 		map[redLoc[0]][redLoc[1]] = 'R';
 		map[blueLoc[0]][blueLoc[1]] = 'B';
+		
+	}
+
+	private static int visitCal(int x1, int y1, int x2, int y2) {
+		return 1000 * x1 + 100 * y1 + 10 * x2 + y2;
+	}
+
+	private static boolean move(char[][] map, int[] newLoc, int d) {
+
+		boolean isGoal = false;
+		boolean aheadMarble = false;
+		
+		int r = newLoc[0];
+		int c = newLoc[1];
+		
+		while(true) {
+			
+			int nr = r + dr[d];
+			int nc = c + dc[d];
+			
+			if(map[nr][nc] == '#') break;
+			if(map[nr][nc] == 'O') {
+				isGoal = true;
+				break;
+			}
+			if(map[nr][nc] == 'B' || map[nr][nc] == 'R' ) {
+				aheadMarble = true;
+			}
+			
+			r = nr;
+			c = nc;
+		}
+		
+		if( aheadMarble ) {
+			
+			if(d == 0) {
+				r += 1;
+			}else if(d == 1) {
+				r -= 1;
+			}else if(d == 2) {
+				c += 1;
+			}else {
+				c -= 1;
+			}
+		}
+		
+		newLoc[0] = r;
+		newLoc[1] = c;
+		
+		return isGoal;
 	}
 }
