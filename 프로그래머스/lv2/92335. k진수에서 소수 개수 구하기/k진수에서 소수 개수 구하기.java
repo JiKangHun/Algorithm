817afb90b95eb4bb3ca506fda0eef4c9208f307f
JiKangHun <97156468+JiKangHun@users.noreply.github.com>
2023-09-06 05:45:49 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 0.07 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
+++ 프로그래머스/lv2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.java
@@ -1,57 +1,20 @@
 import java.util.*;
 
 class Solution {
-    
-    static int answer;
-    
     public int solution(int n, int k) {
-        convert(n, k, "");
-        return answer;
-    }
-    
-    public static void convert(int n, int k, String num){
-        
-        
-        if(n == 0){
-            
-            String part = "";
-            
-            for(int i=0; i<num.length(); i++){
-                
-                char digit = num.charAt(i);
-                if(digit == '0') continue;
-                part+=digit;
-                
-                
-                if(i+1==num.length() || num.charAt(i+1) == '0'){
-                    
-                    long partToLong = Long.parseLong(part);
-                    
-               
-                    boolean isPrimeNumber = true;
-                    
-                    for(int j=2; j<=Math.sqrt(partToLong); j++){
-                        if(partToLong % j == 0){
-                            isPrimeNumber = false;
-                            break;
-                        }
-                    }
-                    if(partToLong != 1 && isPrimeNumber) ++answer;
-                    
-                    part = "";
-                    
-                }
-                
-                
-            }
-            return;
-            
-        }else{
-    
-            convert(n/k, k, n%k + num);
-            
+
+        int ans = 0;
+        String temp[] = Integer.toString(n, k).split("0");
+
+        Loop : for(String t : temp) {
+            if(t.length() == 0) continue;
+            long a = Long.parseLong(t);
+            if(a == 1) continue;
+            for(int i=2; i<=Math.sqrt(a); i++)
+                if(a%i == 0) continue Loop;
+
+            ans++;
         }
-        
-        
+        return ans;
     }
 }
