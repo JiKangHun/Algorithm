b0490dfae980d1d1eca60a046ae917d7d0f25af8
JiKangHun <97156468+JiKangHun@users.noreply.github.com>
2023-09-17 22:15:07 +0900

    [Bronze V] Title: A+B, Time: 104 ms, Memory: 12852 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int A = 0;
+        int B = 0;
+        while(0>=A || B>=10){
+        A = sc.nextInt(); B = sc.nextInt();        
+        }
+        System.out.print(A+B);
+    }
+}
+    
