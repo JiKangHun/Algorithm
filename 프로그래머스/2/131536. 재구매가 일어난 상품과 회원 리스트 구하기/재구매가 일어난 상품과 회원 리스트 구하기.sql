c9763dc8f365cf7a4ea4da0af3e178e18c852c7b
JiKangHun <97156468+JiKangHun@users.noreply.github.com>
2023-10-21 10:46:32 +0900

    [level 2] Title: 재구매가 일어난 상품과 회원 리스트 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
+++ 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -3,3 +3,7 @@ FROM ONLINE_SALE
 GROUP BY USER_ID, PRODUCT_ID
 HAVING COUNT(SALES_DATE) > 1
 ORDER BY USER_ID, PRODUCT_ID DESC;
+
+# 회원ID, 상품ID, 동일한 날짜 조합이 여러개라면?
+# 지금은 COUNT(SALES_DATE)가 구매한 날짜의 수를 의미하지만
+# 그때는 해당 상품을 구매한 횟수는 많지만 구매한 날짜의 수는 아니게 됨.
